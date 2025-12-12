2a75cc7b45ba7888eac4ae40013a721eafc81779
Giuliano Testa <giulianotesta15@gmail.com>
2025-12-12 16:51:30 -0300

    feature: Route and Function for create-incident

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from database import db
+
+class incident(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    severity = db.Column(db.String(20), nullable=False)
+    status = db.Column(db.String(20), nullable=False, default='Open')
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from models import incident
+from database import db
 
 bp = Blueprint('bp', __name__)
+
+@bp.route('/')
+def root():
+    return redirect(url_for('bp.incidents'))
 
 @bp.route('/about')
 def about():
@@ -8,5 +14,24 @@ def about():
 
 @bp.route('/create-incident', methods=['GET', 'POST'])
 def create_incident():
+    if request.method == 'POST':
+        tittle=request.form['title']
+        description=request.form['description']
+        severity=request.form['severity']
+        if not tittle or not description or not severity:
+            flash("All fields are required!", "error")
+            return render_template('create-incident.html')
+        else:
+            new_incident = incident(title=tittle, description=description, severity=severity)
+            db.session.add(new_incident)
+            db.session.commit()
+            flash("Incident created successfully!", "success")
+            return redirect('create-incident.html')
+    
+
+
     return render_template('create-incident.html')
 
+@bp.route('/incidents')
+def incidents():
+    return render_template('incidents.html')
