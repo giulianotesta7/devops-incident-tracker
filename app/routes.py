b724e81d1180cb2967f34f9279b5cd57713662da
Giuliano Testa <giulianotesta15@gmail.com>
2025-12-12 16:17:25 -0300

    feature: create-incident route

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,10 @@ from flask import Blueprint, render_template
 bp = Blueprint('bp', __name__)
 
 @bp.route('/about')
-def index():
+def about():
     return render_template('about.html')
 
-#@bp.route('/create_incident')
-#def incident():
-#    return render_template('incident.html')
+@bp.route('/create-incident', methods=['GET', 'POST'])
+def create_incident():
+    return render_template('create-incident.html')
 
