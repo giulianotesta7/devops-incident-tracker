b2c3b99a8eb9d2f84c7c2234328d37ad8fa04d7e
Giuliano Testa <giulianotesta15@gmail.com>
2025-12-12 16:02:19 -0300

    feature: About route and Base template

--- app/routes.py
+++ app/routes.py
@@ -2,3 +2,11 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('bp', __name__)
 
+@bp.route('/about')
+def index():
+    return render_template('about.html')
+
+#@bp.route('/create_incident')
+#def incident():
+#    return render_template('incident.html')
+
