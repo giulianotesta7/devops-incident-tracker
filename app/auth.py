b36c43118a48dd1b7afdc88b4736f7fdb2de4ac2
Giuliano Testa <giulianotesta15@gmail.com>
2025-12-16 11:56:30 -0300

    feat: sign-up and login routes

--- app/auth.py
+++ app/auth.py
@@ -2,3 +2,10 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    return render_template('sign-up.html')
