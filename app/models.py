57d2ab84f6889abae888ca131f97cfe22eca6677
Giuliano Testa <giulianotesta15@gmail.com>
2025-12-18 00:59:54 -0300

    feat: Assign incident to user (#13)
    * feat: add Creator to incidents
    
    * feat: show creator
    
    * feat: Filter only your incidents
    
    * feat: Assigne user to incident
    
    * feat: Show assigne, assign when creating incident

--- app/models.py
+++ app/models.py
@@ -3,25 +3,24 @@ from flask_login import UserMixin
 
 class Incident(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    created_by_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False, index=True)
+    assigned_to_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=True, index=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
     severity = db.Column(db.String(20), nullable=False)
     status = db.Column(db.String(20), nullable=False, default='Open')
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
-    # 1 -> Many
-    comments = db.relationship(
-        "Comment",
-        back_populates="incident",
-        cascade="all, delete-orphan",
-        lazy="select"
-    )
+
+    comments = db.relationship("Comment",back_populates="incident",cascade="all, delete-orphan",lazy="select")
+    creator = db.relationship("User", foreign_keys=[created_by_id], back_populates="created_incidents")
+    assignee = db.relationship("User", foreign_keys=[assigned_to_id],back_populates="assigned_incidents")
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     incident_id = db.Column(db.Integer, db.ForeignKey("incident.id"), nullable=False, index=True)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
-    # Many -> 1
+
     incident = db.relationship("Incident", back_populates="comments")
 
 class User(UserMixin, db.Model):
@@ -29,4 +28,6 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     name = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    
+
+    created_incidents  = db.relationship("Incident", foreign_keys="Incident.created_by_id", back_populates="creator")
+    assigned_incidents = db.relationship("Incident", foreign_keys="Incident.assigned_to_id", back_populates="assignee")
